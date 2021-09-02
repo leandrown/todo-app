c023d71953cc85e4a3fa4fadf6b5137198e2c6a8
Leandro Vieira <leandrown@outlook.com>
2021-09-02 17:46:41 -0300

    Aulas 1 - 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-app';
+  // title = 'todo-app';
+  public todos: Todo[] = [];
+  public title: string = 'Minhas tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'Ir ao mercado', true));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', false));
+  }
+
+  remove(task: Todo) {
+    const index = this.todos.indexOf(task);
+    if (index != -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(task: Todo) {
+    task.done = true;
+  }
+
+  markAsRedo(task: Todo) {
+    task.done = false;
+  }
 }
